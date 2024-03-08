754cacb06a0a089768a0e13d150a9a1d6f41a47a
dml-vivek <vivelece2020@gmail.com>
2024-03-08 18:27:55 +0530

    base class

--- src/main/java/com/umang/base/Base.java
+++ src/main/java/com/umang/base/Base.java
@@ -1,5 +1,55 @@
 package com.umang.base;
 
-public class Base {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
 
-}
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Base {
+	public static Properties prop;
+	public static WebDriver driver;
+
+	public void loadconfig() throws IOException {
+		
+		Properties prop=new Properties();
+		
+		FileInputStream ip= new FileInputStream(System.getProperty("user.dir")+"configuration\\config.properties");
+		
+		try {
+			prop.load(ip);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	public static void launchapp() {
+		WebDriverManager.chromedriver().setup();
+		String browserName = prop.getProperty("browser");
+		if (browserName.contains("Chrome")) {
+			driver = new ChromeDriver();
+		} else if (browserName.contains("FireFox")) {
+			driver=new FirefoxDriver();
+		
+		}else if (browserName.contains("IE")) {
+			driver=new InternetExplorerDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
+		driver.get("url");
+	}
+
+	
+	
+
+	
+		
+	}}
