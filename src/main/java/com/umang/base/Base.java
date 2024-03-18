b55f16ed9e2a09b8e6cfb012ea5ec6e89eb49c2c
dml-vivek <vivelece2020@gmail.com>
2024-03-18 18:13:11 +0530

    base class fine now

--- src/main/java/com/umang/base/Base.java
+++ src/main/java/com/umang/base/Base.java
@@ -10,46 +10,50 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Base {
-	public static Properties prop;
-	public static WebDriver driver;
+    public static Properties prop;
+    public static WebDriver driver;
 
-	public void loadconfig() throws IOException {
-		
-		Properties prop=new Properties();
-		
-		FileInputStream ip= new FileInputStream(System.getProperty("user.dir")+"configuration\\config.properties");
-		
-		try {
-			prop.load(ip);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    @BeforeTest
+    public void loadconfig() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(
+                    System.getProperty("user.dir") + "\\Configuration\\config.properties");
+            prop.load(ip);
 
-	public static void launchapp() {
-		WebDriverManager.chromedriver().setup();
-		String browserName = prop.getProperty("browser");
-		if (browserName.contains("Chrome")) {
-			driver = new ChromeDriver();
-		} else if (browserName.contains("FireFox")) {
-			driver=new FirefoxDriver();
-		
-		}else if (browserName.contains("IE")) {
-			driver=new InternetExplorerDriver();
-		}
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
-		driver.get("url");
-	}
+           // System.out.print("ip");
+           // System.out.print(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	
-	
+    @Test
+    public void launchapp() {
+        WebDriverManager.chromedriver().setup();
+        String browserName = prop.getProperty("browser");
+        
+        //System.out.print("browserNAME");
+       // System.out.print(browserName);
 
-	
-		
-	}}
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("FireFox")) {
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("IE")) {
+            driver = new InternetExplorerDriver();
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
+        driver.get(prop.getProperty("url"));
+    }
+}
--- src/test/java/com/umang/testcase/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.umang.testcase;
